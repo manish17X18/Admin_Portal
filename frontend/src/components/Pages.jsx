adb719b2bb668d353bc6813679cc8375c49d127f
manish17x18 <manishdesh1710@gmail.com>
2026-07-30 19:47:26 +0530

    Routes

--- frontend/src/components/Pages.jsx
+++ frontend/src/components/Pages.jsx
@@ -2,16 +2,39 @@ import React from 'react'
 import SignIn from './SignIn'
 import Users from './Users'
 import Admins from './Admins'
-import { Navigate, NavLink,Route,Routes } from 'react-router'
+import SidePanel from './SidePanel'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Role from './Role'
+import ProtectedRoute from './ProtectedRoute'
+
 const Pages = () => {
+  const location = useLocation();
+  const noSidePanel = ['/signIn', '/signin', '/'];
+  const showSidePanel = !noSidePanel.includes(location.pathname);
+
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Navigate to="/signIn" replace/>}/>
-        <Route path="/signIn" element={<SignIn/>}/>
-        <Route path="/users" element={<Users/>}/>
-        <Route path="/admins" element={<Admins/>}/>
-      </Routes>
+    <div className="flex">
+      {showSidePanel && <SidePanel />}
+      
+      <div className="flex-1">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Navigate to="/signIn" replace />} />
+          <Route path="/signIn" element={<SignIn />} />
+
+          {/* Protected Routes: Blocks unauthenticated users from URL tampering */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/admins" element={<Admins />} />
+            <Route path="/roles" element={<Role />} />
+          </Route>
+
+          {/* Fallback Catch-All */}
+          <Route path="*" element={<Navigate to="/signIn" replace />} />
+        </Routes>
+      </div>
     </div>
   )
 }
