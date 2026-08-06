1199323b24178eaf77eabe01696c7d5b29f1efcc
manish17x18 <manishdesh1710@gmail.com>
2026-08-06 15:16:04 +0530

    Dashboard details for each realm

--- backend/controllers/dashboard_stats/dashboardController.js
+++ backend/controllers/dashboard_stats/dashboardController.js
@@ -1,28 +1,29 @@
 const { getAdminClient } = require('../../config/database');
-
+const getTargetRealm=require('../realms/helper')
 exports.getDashboardStats = async (req, res) => {
     try {
         const kcClient = await getAdminClient();
+        const realm = getTargetRealm(req);
 
         //  Fetch live total users count
-        const totalUsers = await kcClient.users.count();
+        const totalUsers = await kcClient.users.count({ realm });
 
         //  Fetch all users to calculate admins and user roles distribution
-        const allUsers = await kcClient.users.find();
+        const allUsers = await kcClient.users.find({ realm });
 
         // Fetch all realm roles
-        const roles = await kcClient.roles.find();
+        const roles = await kcClient.roles.find({ realm });
         const totalRoles = roles.length;
 
-        // Fetch admin users to get actual admin count
-        const adminUsers = await kcClient.roles.findUsersWithRole({ name: 'admin' });
+        // Fetch admin users to get actual admin count  
+        const adminUsers = await kcClient.roles.findUsersWithRole({ name: 'admin', realm });
         const totalAdmins = adminUsers.length;
 
         //  Calculate Users by Role dynamically
         // Counting how many users belong to each role
         const roleDistribution = await Promise.all(
             roles.map(async (role) => {
-                const usersInRole = await kcClient.roles.findUsersWithRole({ name: role.name });
+                const usersInRole = await kcClient.roles.findUsersWithRole({ name: role.name, realm });
                 return {
                     name: role.name,
                     count: usersInRole.length,
