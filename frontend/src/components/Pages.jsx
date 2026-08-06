eaf9b59e17321b5fdf80962f90aa1dfca4e3614b
manish17x18 <manishdesh1710@gmail.com>
2026-08-06 15:28:07 +0530

    Updated

--- frontend/src/components/Pages.jsx
+++ frontend/src/components/Pages.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import SignIn from './SignIn'
-import Users from './Users'
-import Admins from './Admins'
-import SidePanel from './SidePanel'
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
-import Dashboard from './Dashboard'
-import Role from './Role'
-import ProtectedRoute from './ProtectedRoute'
+import React from 'react';
+import SignIn from './SignIn';
+import Users from './Users';
+import Admins from './Admins';
+import SidePanel from './SidePanel';
+import RealmsManager from './RealmsManager'; 
+import { RealmProvider } from '../components/RealmContext'; 
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Role from './Role';
+import ProtectedRoute from './ProtectedRoute';
 
 const Pages = () => {
   const location = useLocation();
@@ -14,29 +16,32 @@ const Pages = () => {
   const showSidePanel = !noSidePanel.includes(location.pathname);
 
   return (
-    <div className="flex">
-      {showSidePanel && <SidePanel />}
-      
-      <div className="flex-1">
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Navigate to="/signIn" replace />} />
-          <Route path="/signIn" element={<SignIn />} />
+    <RealmProvider>
+      <div className="flex">
+        {showSidePanel && <SidePanel />}
+        
+        <div className="flex-1">
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Navigate to="/signIn" replace />} />
+            <Route path="/signIn" element={<SignIn />} />
 
-          {/* Protected Routes: Blocks unauthenticated users from URL tampering */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/admins" element={<Admins />} />
-            <Route path="/roles" element={<Role />} />
-          </Route>
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/realms" element={<RealmsManager />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/admins" element={<Admins />} />
+              <Route path="/roles" element={<Role />} />
+            </Route>
 
-          {/* Fallback Catch-All */}
-          <Route path="*" element={<Navigate to="/signIn" replace />} />
-        </Routes>
+            {/* Fallback Catch-All */}
+            <Route path="*" element={<Navigate to="/signIn" replace />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  )
-}
+    </RealmProvider>
+  );
+};
 
-export default Pages
+export default Pages;
