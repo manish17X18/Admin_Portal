e3fe309ad5d6be589bca00763d12457a49e21a58
manish17x18 <manishdesh1710@gmail.com>
2026-08-18 00:52:37 +0530

    Added Proteted Routes

--- frontend/src/components/Pages.jsx
+++ frontend/src/components/Pages.jsx
@@ -3,11 +3,12 @@ import SignIn from './SignIn';
 import Users from './Users';
 import Admins from './Admins';
 import SidePanel from './SidePanel';
-import RealmsManager from './RealmsManager'; 
-import { RealmProvider } from '../components/RealmContext'; 
+import RealmsManager from './RealmsManager';
+import { RealmProvider } from '../components/RealmContext';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Role from './Role';
+import ProtectedRoute from './ProtectedRoute';
 
 const Pages = () => {
   const location = useLocation();
@@ -18,20 +19,28 @@ const Pages = () => {
     <RealmProvider>
       <div className="flex">
         {showSidePanel && <SidePanel />}
-        
+
         <div className="flex-1">
           <Routes>
-            {/* Direct Routes (Protected wrappers removed) */}
-            <Route path="/" element={<Navigate to="/signIn" replace />} />
-            <Route path="/signIn" element={<SignIn />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/realms" element={<RealmsManager />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/admins" element={<Admins />} />
-            <Route path="/roles" element={<Role />} />
 
-            {/* Fallback Catch-All */}
+            {/* Public route */}
+            <Route path="/signIn" element={<SignIn />} />
+
+            {/* Protected routes */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/realms" element={<RealmsManager />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/admins" element={<Admins />} />
+              <Route path="/roles" element={<Role />} />
+            </Route>
+
+            {/* Root */}
+            <Route path="/" element={<Navigate to="/signIn" replace />} />
+
+            {/* Fallback */}
             <Route path="*" element={<Navigate to="/signIn" replace />} />
+
           </Routes>
         </div>
       </div>
